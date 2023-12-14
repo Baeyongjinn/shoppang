656f94b1769ddbab0d8ed54b1c3218b5344b5631
배용진 <dydwls2270@naver.com>
2023-12-14 09:42:05 +0900

    6-1

--- src/main/resources/sqlqurry.sql
+++ src/main/resources/sqlqurry.sql
@@ -18,7 +18,7 @@ CREATE TABLE t_purchase_product(
     ,product_nm VARCHAR(20) NOT NULL
     ,memo VARCHAR(100)
     ,product_count SMALLINT(2) UNSIGNED DEFAULT 1
-    ,create_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    ,created_at DATETIME DEFAULT CURRENT_TIMESTAMP
     ,updated_at DATETIME ON UPDATE CURRENT_TIMESTAMP
     ,buying_check TINYINT(1) NOT NULL DEFAULT 0 CHECK(buying_check IN(0,1,2))
     ,buying_date DATE
