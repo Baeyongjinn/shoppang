41a85a31080471e105be2a390e46ee24438994cb
배용진 <dydwls2270@naver.com>
2023-12-20 14:07:43 +0900

    8-6

--- src/main/java/com/green/projectex/errortest/ErrorCode.java
+++ src/main/java/com/green/projectex/errortest/ErrorCode.java
@@ -2,17 +2,17 @@ package com.green.projectex.errortest;
 
 public class ErrorCode {
 
-    public final static String NULL_USER_ERROR = "존재하지 않는 유저 입니다";
-    public final static String CATEGORY_ERROR = "존재하지 않는 카테고리 번호 입니다";
-    public final static String LIST_ERROR = "잘못된 리스트 번호가 입력 되었습니다";
-    public final static String PRODUCT_ERROR = "잘못된 물건Pk가 입력 되었습니다";
-    public final static String BUYING_CHECK_ERROR = "구매 예정 상품이 아닙니다";
-    public final static String CHECK_USER_ERROR = "다른 유저의 상품 입니다";
-    public final static String CHECK_UID_ERROR = "이미 존재 하는 아이디 입니다";
-    public final static String SIGNIN_UPW_ERROR = "비밀번호가 다릅니다";
-    public final static String SIGNIN_UID_ERROR = "아이디가 다릅니다";
-    public final static String CATEGORY_CHECK_ERROR = "동일한 카테고리 이름이 있습니다";
-    public final static String NULL_CATEGORY_ERROR = "카테고리 이름을 입력 하세요";
+    public static final  String NULL_USER_ERROR = "존재하지 않는 유저 입니다";
+    public static final  String CATEGORY_ERROR = "존재하지 않는 카테고리 번호 입니다";
+    public static final  String LIST_ERROR = "잘못된 리스트 번호가 입력 되었습니다";
+    public static final  String PRODUCT_ERROR = "잘못된 물건Pk가 입력 되었습니다";
+    public static final  String BUYING_CHECK_ERROR = "구매 예정 상품이 아닙니다";
+    public static final  String CHECK_USER_ERROR = "다른 유저의 상품 입니다";
+    public static final  String CHECK_UID_ERROR = "이미 존재 하는 아이디 입니다";
+    public static final  String SIGNIN_UPW_ERROR = "비밀번호가 다릅니다";
+    public static final  String SIGNIN_UID_ERROR = "아이디가 다릅니다";
+    public static final  String CATEGORY_CHECK_ERROR = "동일한 카테고리 이름이 있습니다";
+    public static final  String NULL_CATEGORY_ERROR = "카테고리 이름을 입력 하세요";
 
 
 }
